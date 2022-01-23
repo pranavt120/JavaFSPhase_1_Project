0e97cd1ab3bc90ce70f0b76df2f9dcc6d2ea0a96
pranavt120 <pranavtakkar510@gmail.com>
2022-01-23 18:32:30 +0530

    Sprint 2: Added List all Files Feature

--- src/com/lockedMe/LockedMe.java
+++ src/com/lockedMe/LockedMe.java
@@ -1,11 +1,15 @@
 package com.lockedMe;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class LockedMe {
    private static Scanner scn = new Scanner(System.in);
+   private static  File directoryPath = new File("D:\\java\\File");
+
     public  static void greetingScreen(){
         String  choice ;
         System.out.println("--------------------------------------------------------------------");
@@ -21,6 +25,7 @@ public class LockedMe {
         choice = scn.nextLine();
         while((!choice.equals("1")) || (!choice.equals("2"))){
             if(choice.equals("1")){
+                addDirctoryPath();
                 displayMenu();
                 break;
             }else if( choice.equals("2")){
@@ -35,6 +40,7 @@ public class LockedMe {
     public static void displayMenu(){
         String choice="";
         while(!choice.equalsIgnoreCase("q")){
+            System.out.println("\n\n");
             System.out.println("Please Choose an option ");
             System.out.println("1. List all Files\n" +
                     "2. Add a File\n" +
@@ -64,9 +70,22 @@ public class LockedMe {
 
     }
 
-    public static void listAllFiles(){}
+    public static void listAllFiles(){
+        String contents[] = directoryPath.list();
+        System.out.println("List of all files in "+ directoryPath+" : ");
+        for( var i: contents){
+            System.out.println(i);
+        }
+
+    }
     public static void addFile(){}
     public static void deleteFile(){}
+    public static void addDirctoryPath(){
+        String directory;
+        System.out.println("Enter a valid Directory path you want to Look in:");
+        directory = scn.nextLine();
+        directoryPath = new File(directory);
+    }
 
 
 }
