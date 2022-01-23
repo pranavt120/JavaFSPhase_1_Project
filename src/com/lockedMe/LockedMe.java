593a67ef1cef093887d3c8ee1ebaf12b31ceaae8
pranavt120 <pranavtakkar510@gmail.com>
2022-01-23 21:35:45 +0530

    Sprint 3: Added feature to add file to a directory

--- src/com/lockedMe/LockedMe.java
+++ src/com/lockedMe/LockedMe.java
@@ -1,16 +1,14 @@
 package com.lockedMe;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.InputMismatchException;
-import java.util.Locale;
-import java.util.Scanner;
+import java.io.*;
+import java.nio.file.NoSuchFileException;
+import java.util.*;
 
 public class LockedMe {
    private static Scanner scn = new Scanner(System.in);
    private static  File directoryPath = new File("D:\\java\\File");
 
-    public  static void greetingScreen(){
+    public  static void greetingScreen() throws NullPointerException{
         String  choice ;
         System.out.println("--------------------------------------------------------------------");
         System.out.println("--------------------------------------------------------------------");
@@ -37,7 +35,7 @@ public class LockedMe {
         }
     }
 
-    public static void displayMenu(){
+    public static void displayMenu() throws NullPointerException{
         String choice="";
         while(!choice.equalsIgnoreCase("q")){
             System.out.println("\n\n");
@@ -70,15 +68,64 @@ public class LockedMe {
 
     }
 
-    public static void listAllFiles(){
+    public static void listAllFiles() throws NullPointerException{
         String contents[] = directoryPath.list();
         System.out.println("List of all files in "+ directoryPath+" : ");
+        Arrays.sort(contents);
         for( var i: contents){
             System.out.println(i);
         }
 
     }
-    public static void addFile(){}
+    public static void addFile(){
+        String fileName;
+        String character;
+        FileWriter fout = null;
+        BufferedWriter out = null;
+        System.out.println("Enter the name of the file, you want to add: ");
+        fileName = scn.nextLine();
+        try{
+             fout = new FileWriter(new File(directoryPath+"\\"+fileName));
+             out = new BufferedWriter(fout);
+            System.out.println("Enter the content, you want to add into the file.");
+            System.out.println("Press \"$\" key and Press Enter key to Add file to the Directory. ");
+            while(true){
+                character = scn.nextLine();
+                System.out.println(character.charAt(character.length() -1));
+                if(character.charAt(character.length() -1) == '$'){
+                    out.write(character.substring(0,character.length() -1));
+                    out.flush();
+                    break;
+                }else{
+                    out.write(character+"\n");
+                }
+            }
+            System.out.println("File added successfully");
+
+        }catch (IOException ex){
+            System.out.println("Could not Add file . Check directory path");
+        } catch (NoSuchElementException ne){
+            System.out.println("No such element");
+
+        } finally{
+            if(fout != null){
+                try{
+                    fout.close();
+                }catch (IOException ex){
+                    System.out.println("could not close fout");
+                }
+            }
+            if(out != null){
+                try{
+                    out.close();
+                }catch (IOException ex){
+                    System.out.println("could not close Buffered reader");
+                }
+            }
+
+        }
+
+    }
     public static void deleteFile(){}
     public static void addDirctoryPath(){
         String directory;
--- src/com/lockedMe/Main.java
+++ src/com/lockedMe/Main.java
@@ -1,10 +1,16 @@
 package com.lockedMe;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 
 public class Main {
     public static void main(String[] args) {
+        try{
             LockedMe.greetingScreen();
+        }catch (NullPointerException ex){
+            System.out.println("Enter Correct directory path");
+        }
+
 
 
 
